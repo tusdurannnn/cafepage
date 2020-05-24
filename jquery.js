a7dfae1d9c01657e634112e252f83e64c1475560
tusdurannnn <64243456+tusdurannnn@users.noreply.github.com>
2020-05-25 00:17:05 +0900

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -8,11 +8,25 @@ $(function(){
      if ($(window).scrollTop() < imgHeight) {
        //メインビジュアル内にいるので、クラスを外す。
        header.removeClass('headerColor-default');
+         $('a.nav-link').css('color','#fff');
      }else {
          $('a.nav-link').css('color','#000000');
        header.addClass('headerColor-default');
+         $('body,html').css('height','auto');
      }
   });
+    
+    $(function() {
+    $(window).scroll(function (){
+    $('.fadein').each(function(){
+        var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+           windowHeight = $(window).height();
+         if (scroll > elemPos - windowHeight + 100){
+             $(this).addClass('scrollin');
+         }
+        });
+    });
+});
 
-  
 });
